116c0d41b3423b31a59cdca2ecc4a64017c00151
watackeech <devkeech@gmail.com>
2024-09-07 13:03:55 +0900

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { Canvas } from '@react-three/fiber'
+import { XR, createXRStore } from '@react-three/xr'
+import { useState } from 'react'
+
+const store = createXRStore()
+
+export default function Home() {
+  const [red, setRed] = useState(false)
+  return (
+    <>
+      <button onClick={() => store.enterAR()}>Enter AR</button>
+      <Canvas>
+        <XR store={store}>
+          <mesh pointerEventsType={{ deny: 'grab' }} onClick={() => setRed(!red)} position={[0, 1, -1]}>
+            <boxGeometry />
+            <meshBasicMaterial color={red ? 'red' : 'blue'} />
+          </mesh>
+        </XR>
+      </Canvas>
+    </>
+  )
+}
